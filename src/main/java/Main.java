c1fc2e697a2791090df37c21730d5db7ace00a6e
OmarKayed <omar2346@stud.kea.dk>
2022-09-26 12:12:53 +0200

    1. merge øvelse

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,8 @@ public class Main {
         System.out.println("Hejj");
         System.out.println("Farvel");
         System.out.println("Din mor ligner en mand");
+        System.out.println("Din mor og far er i samme familie");
+        System.out.println("Tak for i dag");
+
     }
 }
