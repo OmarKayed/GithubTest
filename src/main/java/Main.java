ed7cb70a10d5938115ddf3847c5e9f885e0bd1e5
Teller501 <ande777j@stud.kea.dk>
2022-09-26 12:10:47 +0200

    2. merge øvelse

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hejj");
         System.out.println("Farvel");
         System.out.println("Din mor er en mand");
+        System.out.println("Jeg scorer damer på github");
     }
 }
