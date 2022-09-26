1e2e69df8641dffa0d05c18505fd4deab9cf918b
OmarKayed <omar2346@stud.kea.dk>
2022-09-26 12:12:09 +0200

    1. merge øvelse

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hejj");
         System.out.println("Farvel");
+        System.out.println("Din mor ligner en mand");
     }
 }
