24460402cdd5ef60efd75c52b2f20d22e31de90e
Teller501 <ande777j@stud.kea.dk>
2022-09-26 12:11:43 +0200

    4. merge øvelse

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Din mor er en mand");
         System.out.println("Jeg scorer damer på github");
         System.out.println("fejl");
+        System.out.println("fejl 2");
     }
 }
